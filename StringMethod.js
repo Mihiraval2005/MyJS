09c79fa018184bc9546afa67c373a9739c87e484
MIHIR C RAVAL <145988918+Mihiraval2005@users.noreply.github.com>
2025-01-12 10:25:59 +0530

    Add files via upload

---
+++ StringMethod.js
@@ -0,0 +1,95 @@
+//charAt()
+//Returns the character at a specific index in a string.
+// let st = "MIHIR";
+// console.log(st.charAt(4));
+
+// charCodeAt()
+//Returns the Unicode (ASCII) value of the character at a specific index.
+// let mcr = 'raval';
+// console.log(mcr.charCodeAt(3));
+
+//concat()
+//Joins two or more strings into one.
+// let a  = 'js';
+// let b  = ' react';
+
+// let ans = a.concat(b);
+// console.log(ans)
+
+//includes()
+//Checks if a string contains a specified substring.
+// let a = 'react js';
+// console.log(a.includes('jss'));
+
+//indexOf()
+//Returns the index of the first occurrence of a specified substring.
+// let a = 'demo work';;
+// console.log(a.indexOf('work'))
+
+//lastIndexOf()
+//Returns the index of the last occurrence of a specified substring.
+// let mr = 'he he je';
+// console.log(mr.lastIndexOf('je')) 
+
+//match()
+//Retrieves matches based on a regular expression.
+// let str = "The quick brown fox";
+// let result = str.match(/\b\w+\b/g); // matches all words
+// console.log(result);
+
+//replace()
+//Replaces part of a string with another string.
+// let a = 'hy mihir';
+// let b = a.replace('hy','raval');
+// console.log(b)
+
+//search()
+//Executes a search for a match using a regular expression.
+// let a = 'mihir';
+// console.log(a.search('i'));
+
+//slice()
+//Extracts a section of a string, starting from a specific index.
+// let str = "Hello World";
+// console.log(str.slice(0, 6)); // "Hello"
+// console.log(str.slice(6)); // "World"
+
+//split()
+//Splits a string into an array of substrings.
+// let m = "Mihir raval";
+// console.log(m.split(" "))
+
+//startsWith()
+//Checks if a string starts with a specified substring.
+// let a = "mi vi";
+// console.log(a.startsWith('vi'))
+
+//substr()
+//Extracts a substring from a string, starting at a specified index (and with a specific length).
+// let str = "Hello World";
+// console.log(str.substr(4, 1)); // "Hello"
+// console.log(str.substr(6, 5)); // "World"
+
+//substring()
+//Extracts characters from a string between two specified indices.
+// let sr = 'ndnsd kskds';
+// let mr = sr.substring(11)
+// console.log(mr)
+
+//toLowerCase()
+//Converts a string to lowercase.
+// let a = 'MCR';
+// console.log(a.toLowerCase());
+
+//toUpperCase()
+//Converts a string to uppercase.
+// let a = 'mcr';
+// console.log(a.toUpperCase());
+
+// trim()
+//Removes whitespace from both ends of a string.
+// let a = ' fsdfsd ff fsf ';
+// console.log(a.trim());
+
+
+
